fc5460b0aecf23200723e18a902859f2c694ed92
deonj197 <deonj197@gmail.com>
2017-02-09 14:56:24 -0500

    corrected header

--- chess.cpp
+++ chess.cpp
@@ -1,5 +1,7 @@
 #include<iostream>   
-using std::cout; using std::cin; using std::endl; using std::string;
+using std::cout; using std::cin; using std::endl; 
+#include<string>
+using std::string;
 bool valid_move_pawn(){
 	return true;
 }
