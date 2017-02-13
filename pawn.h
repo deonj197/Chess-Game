c0f07c4def471316c35b20e4904cd3468f790037
Deon Jackson Jr <deonj197@gmail.com>
2017-02-13 13:21:37 -0500

    added functions and data member declarations

--- pawn.h
+++ pawn.h
@@ -1,11 +1,23 @@
+/*
+ * pawn.h
+ *
+ *  Created on Feb 13, 2017
+ * 		Author: Deon
+ */
+
+
 #ifndef PAWN_H
 #define PAWN_H
 
 class pawn {
 	public:
-		pawn(); //pawn constructor
+		pawn(char set_color); // pawn constructor
+	
+		bool valid_move_pawn(long vert, long horiz); // move error-checking function
 	protected:
 	private:
+		char color; // holds the pieces color (white or black = w or b)
+		
 };
 
 #endif // PAWN_H
