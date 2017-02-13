8e2a03ea6119ccf79849d88d8451054db3a922ce
Deon Jackson Jr <deonj197@gmail.com>
2017-02-13 13:21:15 -0500

    added function and constructor definitions

--- pawn.cpp
+++ pawn.cpp
@@ -1,8 +1,18 @@
+/*
+ * pawn.cpp
+ *
+ *  Created on Feb 13, 2017
+ * 		Author: Deon
+ */
 #include <iostream>
 #include "pawn.h"
 
 using namespace std;
 
-pawn::pawn(){
+pawn::pawn(char set_color):color(set_color){ 
+}
 
+bool pawn::valid_move_pawn(long vert, long horiz){
+	
+	return true;
 }
